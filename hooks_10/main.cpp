468e66d178caf7c8b7e3c55824984c1272e250e1
Luca Miniati <luca.miniati@gmail.com>
2024-03-22 20:04:42 -0400

    out

--- hooks_10/main.cpp
+++ hooks_10/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <fstream>
+#include <utility>
+#include <string>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
 using namespace std;
 using u64 = unsigned long long int;
+using grid = pair<u64, u64>;
 
-struct grid {
-    u64 values[2] = {0ull, 1ull};
-};
+ofstream fout("hooks.out");
 int combos = 0;
 auto start = chrono::steady_clock::now();
 
@@ -18,28 +19,48 @@ const u64 hooks[n][4][2] = {
     {{262663ull, 0ull}, {1050631ull, 0ull}, {1835521ull, 0ull}, {1837060ull, 0ull}},
     {{134480399ull, 0ull}, {1075843087ull, 0ull}, {2013528577ull, 0ull}, {2015367176ull, 0ull}},
     {{68853957151ull, 0ull}, {1101663313951ull, 0ull}, {2130438259201ull, 0ull}, {2132455464976ull, 0ull}},
-    {{35253226046015ull, 0ull}, {1128103233470527ull, 0ull}, {1090784388710913ull, 0ull}, {2218818768224288ull, 0ull}},
-    {{18049651735528063ull, 0ull}, {1128103233470527ull, 0ull}, {2216684295553537ull, 0ull}, {2218818768224288ull, 0ull}},
-    {{0ull, 0ull}, {0ull, 0ull}, {0ull, 0ull}, {0ull, 0ull}},
-    {{0ull, 0ull}, {0ull, 0ull}, {0ull, 0ull}, {0ull, 0ull}},
+    {{35253226046015ull, 0ull}, {1128103233470527ull, 0ull}, {2216684295553537ull, 0ull}, {2218818768224288ull, 0ull}},
+    {{18049651735528063ull, 0ull}, {1155177711073788031ull, 0ull}, {2287863863930257921ull, 0ull}, {2290084817171152960ull, 0ull}},
+    {{9241421688590303999ull, 0ull}, {2310355422147576063ull, 64ull}, {9241421688590303745ull, 127ull}, {11533727459002351744ull, 127ull}},
+    {{9241421688590304255ull, 256ull}, {4620710844295152127ull, 65664ull}, {9241421688590303745ull, 130816ull}, {4620710844295151872ull, 130944ull}},
 };
 
-void print_grid(grid &grid, int val) {
-    cout << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "  ";
-        for (int j = 0; j < n; j++) {
-            int index = i * n + j;
-            int board_index = index / 64;
-            int bit_index = index % 64;
-            if (grid.values[board_index] & (1ull << bit_index)) {
-                cout << val << " ";
-            } else {
-                cout << "0 ";
-            }
+void print_grid(grid &grid, string val) {
+    fout << endl;
+
+    // first 64 bits of grid
+    for (int i = 0; i < 64; i++) {
+        if ((i % 9) == 0)
+            fout << "  ";
+
+        if (grid.first & (1ull << i)) {
+            fout << val << " ";
+        } else {
+            fout << ". ";
         }
-        cout << endl;
+
+        if ((i % 9) == 8)
+            fout << endl;
     }
+
+    // last 17 bits of grid
+    for (int i = 0; i < 17; i++) {
+        if ((i % 9) == 8)
+            fout << "  ";
+
+        if (grid.second & (1ull << i)) {
+            fout << val << " ";
+        } else {
+            fout << ". ";
+        }
+
+        if ((i % 9) == 7)
+            fout << endl;
+    }
+
+    fout << "first: " << grid.first << endl;
+    fout << "second: " << grid.second << endl;
+    fout << endl;
 }
 
 int msb(u64 n) {
@@ -65,30 +86,40 @@ int bitstring_length(u64 n) {
     return length;
 }
 
-void set_bits(grid &grid, u64 bitmask[], int shift) {
-    grid.values[0] |= bitmask[0] << shift;
+void set_bits(grid &grid, u64 first, u64 second, int shift) {
+    grid.first |= first << shift;
     // if we try to shift into index 64 or higher
-    if ((msb(bitmask[0]) + shift) > 63) {
-        cout << "overflow" << endl;
-        cout << (bitmask[1] >> (64 - shift)) << endl;
-        grid.values[1] |= (bitmask[0] >> (64 - shift));
+    if ((msb(first) + shift) > 64) {
+        grid.second |= (first >> (64 - shift));
     }
+    grid.second |= second << shift;
 }
 
-void unset_bits(grid &grid, u64 bitmask, int shift) {
-    grid.values[0] &= ~(bitmask << shift);
+void unset_bits(grid &grid, u64 first, u64 second, int shift) {
+    grid.first &= ~(first << shift);
     // if we try to shift into index 64 or higher
-    if ((msb(bitmask) + shift) > 63) {
-        grid.values[1] &= ~(bitmask >> (64 - shift));
+    if ((msb(first) + shift) > 64) {
+        grid.second |= (first >> (64 - shift));
     }
+    grid.second &= ~(second << shift);
 }
 
 void set_hook(grid &grid, int x, int y, int hook_size, int rotation) {
-    set_bits(grid, hooks[hook_size][rotation], (y * 9) + x);
+    set_bits(
+        grid,
+        hooks[hook_size-1][rotation][0],
+        hooks[hook_size-1][rotation][1],
+        (y * 9) + x
+    );
 }
 
 void unset_hook(grid &grid, int x, int y, int hook_size, int rotation) {
-    unset_bits(grid, hooks[hook_size][rotation], (y * 9) + x);
+    unset_bits(
+        grid,
+        hooks[hook_size-1][rotation][0],
+        hooks[hook_size-1][rotation][1],
+        (y * 9) + x
+    );
 }
 
 // bool valid(grid &grid, int x, int y, int hook_size, int rotation) {
@@ -148,46 +179,31 @@ void unset_hook(grid &grid, int x, int y, int hook_size, int rotation) {
 // }
 
 int main() {
+
     grid grid;
+    grid.first = 0ull;
+    grid.second = 0ull;
 
-    // int hook_size = 5;
-    // int x = 0, y = 0;
-    // for (int x = 0; x < n - 6; x++) {
-    //     for (int y = 0; y < n - 6; y++) {
-            // for (int r = 0; r < 4; r++) {
-            //     set_hook(grid, x, y, hook_size-1, r);
-            //
-            //     print_grid(grid, 1);
-            //
-            //     unset_hook(grid, x, y, hook_size-1, r);
-            // }
-    //     }
-    // }
+    for (int h = 3; h < 4; h++) {
+        for (int x = 0; x < n - h + 1; x++) {
+            for (int y = 0; y < n - h + 1; y++) {
+                for (int r = 0; r < 4; r++) {
+                    set_hook(grid, x, y, h, r);
 
-    cout << endl;
-    u64 bitmask = 
-        (1ull << (0 + 0 * n)) |
-        (1ull << (1 + 0 * n)) |
-        (1ull << (2 + 0 * n)) |
-        (1ull << (3 + 0 * n)) |
-        (1ull << (4 + 0 * n)) |
-        (1ull << (5 + 0 * n)) |
-        (1ull << (6 + 0 * n)) |
-        (1ull << (7 + 0 * n)) |
-        (1ull << (0 + 1 * n)) |
-        (1ull << (0 + 2 * n)) |
-        (1ull << (0 + 3 * n)) |
-        (1ull << (0 + 4 * n)) |
-        (1ull << (0 + 5 * n)) |
-        (1ull << (0 + 6 * n)) |
-        (1ull << (63));
-    // set_bits(grid, bitmask, 0);
-    print_grid(grid, 1);
-    cout << bitmask << endl;
+                    print_grid(grid, "x");
+
+                    unset_hook(grid, x, y, h, r);
+
+                    if (h == 1)
+                        break;
+                }
+            }
+        }
+    }
 
     // auto end = chrono::steady_clock::now();
     // chrono::duration<double> duration = end - start;
-    // cout << endl;
-    // cout << "Total Combinations: " << combos << endl;
-    // cout << "Execution time: " << round(duration.count() * 100) / 100 << "s" << endl;
+    // fout << endl;
+    // fout << "Total Combinations: " << combos << endl;
+    // fout << "Execution time: " << round(duration.count() * 100) / 100 << "s" << endl;
 }
